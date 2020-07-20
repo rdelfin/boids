09767e36e41560bbf61e7d1e7f4852bb5fdba36a
Ricardo Delfin <me@rdelfin.com>
2020-07-20 20:35:04 +0100

    Config fixes

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
 
     let resources = app_root.join("resources");
-    let display_config = resources.join("display_config.ron");
+    let config = app_root.join("config");
+    let display_config = config.join("display.ron");
 
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
